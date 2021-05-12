cbb8db6ee8bc8022c4bda8cbc2e3f869d6d7808e
Im2Slothy <69122728+Im2Slothy@users.noreply.github.com>
2021-05-12 12:16:03 -0500

    Add files via upload

---
+++ Speedometer/speedometer.lua
@@ -0,0 +1,25 @@
+-- HELP BY FRIEND FOR VALUES OF SPEED
+--[[ 
+local mph = 2.2369
+local kph = 3.6 
+]]
+
+function text(content) 
+    SetTextFont(2)
+    SetTextProportional(0)
+    SetTextScale(1.9,1.9)
+    SetTextEntry("STRING")
+    AddTextComponentString(content .. " MPH")
+    DrawText(0.8,0.9)
+end
+Citizen.CreateThread(function()
+-- Little bit of help but it makes it so it only shows speed when in vehicle. 
+    while true do
+        Citizen.Wait(1)
+        local speed = (GetEntitySpeed(GetVehiclePedIsIn(GetPlayerPed(-1), false))*2.2369)
+        -- Condition to check if the ped is in a vehicle 
+        if(IsPedInAnyVehicle(GetPlayerPed(-1), false)) then
+            text(math.floor(speed))
+        end
+    end
+end)
